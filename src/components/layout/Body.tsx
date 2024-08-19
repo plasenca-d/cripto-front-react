6fb51945ef2780682e0a47f4d5cc79e612ce9247
Franzua Plasencia <franzua.15pc@gmail.com>
2024-08-18 23:22:58 -0500

    feat(pages): change provider

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -2,9 +2,6 @@ import React, { useEffect } from "react";
 import Sidebar from "@/components/layout/Sidebar";
 import Navbar from "@/components/layout/Navbar";
 import { linkEnum } from "@/types/enums/link";
-import { ChakraProvider } from "@chakra-ui/react";
-
-import { theme } from "@/theme";
 
 interface BodyProps {
   pageName: linkEnum;
@@ -19,17 +16,15 @@ function Body(props: BodyProps) {
   }, [pageName]);
 
   return (
-    <ChakraProvider theme={theme}>
-      <section className="min-h-screen w-full flex">
-        <Sidebar />
-        <main className="w-full md:w-[calc(100%-300px)]">
-          <Navbar />
-          <section className="bg-light p-7 space-y-6 min-h-[calc(100vh-100px)]">
-            {children}
-          </section>
-        </main>
-      </section>
-    </ChakraProvider>
+    <section className="min-h-screen w-full flex">
+      <Sidebar />
+      <main className="w-full md:w-[calc(100%-300px)]">
+        <Navbar />
+        <section className="bg-light p-7 space-y-6 min-h-[calc(100vh-100px)]">
+          {children}
+        </section>
+      </main>
+    </section>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import { theme } from "@/theme";
+
 import "@/styles/main.scss";
 import router from "@/routes";
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
