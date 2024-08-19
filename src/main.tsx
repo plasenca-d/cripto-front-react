4f9c926b6ebca9ab95559b66f2b860d6d013e00c
Franzua Plasencia <franzua.15pc@gmail.com>
2024-08-18 22:33:54 -0500

    feat: add chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router-dom";
 
 import "@/styles/main.scss";
 import router from "@/routes";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
