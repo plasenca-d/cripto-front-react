507eccda6016c51e507f5ce5e8d4aecea950e76f
Ing. Armando Campos <armandocamposf@gmail.com>
2024-08-26 22:53:04 -0500

    Fix: error compilacion

--- src/types/enums/link.ts
+++ src/types/enums/link.ts
@@ -2,4 +2,11 @@ export enum linkEnum {
   Dashboard = 'Inicio',
   Referidos = 'Referidos',
   Setting = 'Configuraciones',
+  Accounts = 'Cuentas',
+  Credit = 'Creditos',
+  Investments = 'Inversiones',
+  Loans = 'Loans',
+  Services = 'Servicios',
+  Transactions = 'Transacciones',
+  
 }
